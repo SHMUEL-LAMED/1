64fa0cd2533b600e5f30b3b47ee0c6dfeed0b8d8
שמואל <0534169095@xn--4dbjbascrao3i.com>
2026-08-13 14:00:48 +0300

    עדכון מטמון לממשק הניהול המתוקן

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // מעטפת האפליקציה והסמל נשמרים לעבודה מהירה וגם במצב לא מקוון.
 // שם המטמון נושא מספר גרסה; העלאת המספר מפילה את הגרסאות הישנות ב-activate.
-const CACHE_VERSION = "v30";
+const CACHE_VERSION = "v31";
 const CACHE_NAME = `simchat-gallery-shell-${CACHE_VERSION}`;
 const CACHE_PREFIX = "simchat-gallery-shell-";
 
